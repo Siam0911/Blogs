0067ed00f47d2e5e64aa280f19891f2f22178f37
Siam0911 <slowmanremember@gmail.com>
2025-06-14 23:09:51 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://Siam0911.github.io",
-	base: "/",
+	base: "/Blogs",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
